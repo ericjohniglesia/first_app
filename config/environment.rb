7f44f56b03e9a83a50366dffc51befd508ac713c
U-ERIC-PC\Eric John Iglesia <Eric John Iglesia@eric-pc.(none)>
2014-02-20 23:10:37 +0800

    Improve production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 FirstApp::Application.initialize!
-FirstApp::Application.configure do
-  .
-  .
-  .
-  config.serve_static_assets = true
-  .
-  .
-  .
-end
